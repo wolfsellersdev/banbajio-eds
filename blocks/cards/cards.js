8908c537f8165d256f5538899cfc4858c9e1ff69
José Alberto Hernández Sánchez <albertohernandez@wolfsellers.com>
2024-04-17 17:43:24 -0600

    Test

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -15,4 +15,5 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+  console.log('test')
 }
